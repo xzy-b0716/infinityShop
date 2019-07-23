91b96964d27537b4150e0fa333cb914a9329e060
ruanyue-ry <ruanyueue_ry@163.com>
2019-07-24 01:03:48 +0800

    feat: 增加购物车基础组件

--- src/main.js
+++ src/main.js
@@ -3,8 +3,15 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import GoodList from './GoodList/GoodList'
+import Header from './components/Header'
+import './assets/icon/iconfont.css'
+import './assets/css/common.css'
+
 
 Vue.config.productionTip = false
+Vue.component("goodlist",GoodList)
+Vue.component("Header",Header)
 
 /* eslint-disable no-new */
 new Vue({
